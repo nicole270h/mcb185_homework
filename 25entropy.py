1e3e8cfce55e15ce30f5d07a7dbcc4a65841950c
nicole270h <nicole@campus-027-195.ucdavis.edu>
2024-07-01 08:01:30 -0700

    update 7/1/2024

--- 25entropy.py
+++ 25entropy.py
@@ -1,14 +1,27 @@
 import math
 def entropy(a,c,t,g):
-	if a != 0 and c !=0 and t != 0 and g != 0:
-		pa = a/(a+c+t+g)
-		pc = c/(a+c+t+g)
-		pt = t/(a+c+t+g)
-		pg = g/(a+c+t+g)
-		shannon = -pa*(math.log2(pa))-pc*(math.log2(pc))-pt*(math.log2(pt))-pg*(math.log2(pg))
-		return shannon
+	pa = a/(a+c+t+g)
+	pc = c/(a+c+t+g)
+	pt = t/(a+c+t+g)
+	pg = g/(a+c+t+g)
+	if a != 0:
+		pax = -pa*(math.log2(pa))
 	else:
-		return "not applicable"
+		pax = 0
+	if c != 0:
+		pcx = -pc*(math.log2(pc))
+	else:
+		pcx = 0
+	if t != 0:
+		ptx = -pt*(math.log2(pt))
+	else:
+		ptx = 0
+	if g != 0:
+		pgx = -pg*(math.log2(pg))
+	else:
+		pgx = 0
+	shannon = pax + pcx + ptx + pgx
+	return shannon
 		
 aden = 2
 cyto = 2
